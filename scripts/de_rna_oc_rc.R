ebc0390e3b60428e7261015cd962c7650649f00b
bjyzj <129678950+bjyzj@users.noreply.github.com>
2026-03-04 17:25:38 +0000

    troubleshoot the tpm pathway for DE

--- scripts/de_rna_oc_rc.R
+++ scripts/de_rna_oc_rc.R
@@ -195,14 +195,21 @@ ovary_gtex_filtered <- ovary_gtex_filtered %>%
 
 anyDuplicated(ovary_gtex_filtered$Geneid)
 
-
 renal_gtex_filtered <- renal_gtex_filtered %>%
   group_by(Geneid) %>%
   summarise(across(everything(), mean, na.rm = TRUE)) %>%
   ungroup()
 
 anyDuplicated(renal_gtex_filtered$Geneid)
+
+
+
 #########################
+# expression matrix of both ovary and renal
+#########################
+
+
+
 # geneid col as non numeric clean for gtex 
 ovary_gtex_filtered <- as.data.frame(ovary_gtex_filtered)
 rownames(ovary_gtex_filtered) <- ovary_gtex_filtered[,1]
@@ -216,47 +223,56 @@ tpm_ccRCC <- as.data.frame(tpm_ccRCC)
 rownames(tpm_ccRCC) <- tpm_ccRCC[,1]
 tpm_ccRCC <- tpm_ccRCC[,-1]
 
-# log2 transform all datasets (after adding pseudocount of 1)
-occc_log <- log2(occc_all + 1)
-ovary_gtex_log <- log2(ovary_gtex_filtered + 1)
-ccrcc_log <- log2(tpm_ccRCC + 1)
-renal_gtex_log <- log2(renal_gtex_filtered + 1)
+# remove extra col info from gtex
+ovary_gtex_filtered <- ovary_gtex_filtered[, !colnames(ovary_gtex_filtered) %in% "gene_biotype"]
+renal_gtex_filtered <- renal_gtex_filtered[, !colnames(renal_gtex_filtered) %in% "gene_biotype"]
 
-######################
-# expression matrix of both ovary and renal
-
-common_genes <- Reduce(intersect, list(
-  rownames(occc_log),
-  rownames(ovary_gtex_log),
-  rownames(ccrcc_log),
-  rownames(renal_gtex_log)
+# combine RAW TPM first (NOT log)
+common_genes_all <- Reduce(intersect, list(
+  rownames(occc_all),
+  rownames(ovary_gtex_filtered),
+  rownames(tpm_ccRCC),
+  rownames(renal_gtex_filtered)
 ))
 
-occc_log <- occc_log[common_genes, ]
-ovary_gtex_log <- ovary_gtex_log[common_genes, ]
-ccrcc_log <- ccrcc_log[common_genes, ]
-renal_gtex_log <- renal_gtex_log[common_genes, ]
-# remove extra col info from gtex
-ovary_gtex_log <- ovary_gtex_log[, !colnames(ovary_gtex_log) %in% "gene_biotype"]
-renal_gtex_log <- renal_gtex_log[, !colnames(renal_gtex_log) %in% "gene_biotype"]
+length(common_genes_all)
 
-expr_mat <- cbind(
-  occc_log,
-  ovary_gtex_log,
-  ccrcc_log,
-  renal_gtex_log
-)
+# group all datasets to common genes
+occc_all <- occc_all[common_genes_all, ]
+ovary_gtex_filtered <- ovary_gtex_filtered[common_genes_all, ]
+tpm_ccRCC <- tpm_ccRCC[common_genes_all, ]
+renal_gtex_filtered <- renal_gtex_filtered[common_genes_all, ]
 
-#saveRDS(expr_mat, file = "/Users/beyzaerkal/Desktop/occc_multi-omics/processed/expression_matrix_occc_renal.rds")
+# combine raw tpm
+expr_raw_oc_rc <- cbind(occc_all, ovary_gtex_filtered, tpm_ccRCC, renal_gtex_filtered)
+dim(expr_raw_oc_rc)
+
+# filter low expression genes
+# keep only genes with tpm >= 1 in at least 6 samples
+keep_genes <- rowSums(expr_raw_oc_rc >= 1) >= 6
+expr_raw_oc_rc_filtered <- expr_raw_oc_rc[keep_genes, ]
+nrow(expr_raw_oc_rc) # 15488
+nrow(expr_raw_oc_rc_filtered) # All 15,488 common genes already have tpm >= 1 in at least 6 samples 
+
+# log2 transform 
+# careful naming on from expr_raw to expr_mat for DE analysis
+expr_mat_oc_rc_filtered <- log2(expr_raw_oc_rc_filtered + 1)
+
+# filter low variance genes - remove genes with zero variance across all samples
+expr_mat_oc_rc_filtered <- expr_mat_oc_rc_filtered[apply(expr_mat_oc_rc_filtered, 1, var) > 0, ]
+nrow(expr_mat_oc_rc_filtered) # 15488
+
+#saveRDS(expr_mat_oc_rc_filtered, file = "/Users/beyzaerkal/Desktop/occc_multi-omics/processed/expression_matrix_occc_renal.rds")
 
 ##############
-samples <- colnames(expr_mat)
+samples <- colnames(expr_mat_oc_rc_filtered)
 
 # GTEx ovary and renal sample IDs - for separate labelling
 
-ovary_samples <- colnames(ovary_gtex_log)
-renal_samples <- colnames(renal_gtex_log)
-ccrcc_samples <- colnames(ccrcc_log)
+ovary_samples <- colnames(ovary_gtex_filtered)
+renal_samples <- colnames(renal_gtex_filtered)
+ccrcc_samples <- colnames(tpm_ccRCC)
+
 
 ea_samples <- c("ES-2", "JHOC-5", "OVISE", "OVMANA", "OVTOKO", "RMG-I", "TOV-21G")
 
@@ -290,15 +306,15 @@ sample_info <- data.frame(
 
 rownames(sample_info) <- sample_info$Sample
 samples[duplicated(samples)] # col checks that have duplicates
-all(colnames(expr_mat) == rownames(sample_info)) 
+all(colnames(expr_mat_oc_rc_filtered) == rownames(sample_info)) 
 
 # Combined group factor - make categorical data combining metadata into groups
 group <- rep(NA, length(samples))
 names(group) <- samples
-group[samples %in% colnames(occc_log)] <- "OCCC"
-group[samples %in% colnames(ovary_gtex_log)] <- "GTEx_Ovary"
-group[samples %in% colnames(ccrcc_log)] <- "TCGA-KIRC" # ccRCC
-group[samples %in% colnames(renal_gtex_log)] <- "GTEx_Renal_Cortex"
+group[samples %in% colnames(occc_all)] <- "OCCC"
+group[samples %in% colnames(ovary_gtex_filtered)] <- "GTEx_Ovary"
+group[samples %in% colnames(tpm_ccRCC)] <- "TCGA-KIRC" # ccRCC
+group[samples %in% colnames(renal_gtex_filtered)] <- "GTEx_Renal_Cortex"
 
 sample_info$Group <- factor(group, levels = c("GTEx_Ovary", "OCCC", "GTEx_Renal_Cortex", "TCGA-KIRC"))
 
@@ -308,8 +324,8 @@ table(sample_info$Tissue, sample_info$Status)
 ##############################
 # gene checks
 # Use top 500 most variable genes 
-top_genes <- head(order(apply(expr_mat, 1, sd), decreasing = TRUE), 500)
-expr_sub <- expr_mat[top_genes, ]
+top_genes <- head(order(apply(expr_mat_oc_rc_filtered, 1, sd), decreasing = TRUE), 500)
+expr_sub <- expr_mat_oc_rc_filtered[top_genes, ]
 
 # Create annotation for columns
 annotation_col <- data.frame(
@@ -330,64 +346,52 @@ pheatmap(expr_sub,
 
 ###############################
 # log2((OCCC / GTEx Ovary) / (ccRCC / GTEx Renal Cortex))
+# check
+any(is.na(sample_info$Group)) #FALSE
+all(colnames(expr_mat_oc_rc_filtered) == rownames(sample_info)) #TRUE
 
+# limma
 design <- model.matrix(~0 + Group, data = sample_info)
 colnames(design) <- levels(sample_info$Group)
 
-contrast.matrix <- makeContrasts(OCCC_vs_ccRCC_ratio = (OCCC - GTEx_Ovary) - (ccRCC - GTEx_Kidney),
+colnames(design) <- make.names(colnames(design)) # for to make the name valid for limma contrasts TCGA-KIRC to TCGA.KIRC
+
+contrast.matrix <- makeContrasts(OCCC_vs_ccRCC_ratio = (OCCC - GTEx_Ovary) - (TCGA.KIRC - GTEx_Renal_Cortex),
                                  levels = design)
 
-fit <- lmFit(expr_mat, design)
+fit <- lmFit(expr_mat_oc_rc_filtered, design)
 fit2 <- contrasts.fit(fit, contrast.matrix)
-fit2 <- eBayes(fit2)
+fit2 <- eBayes(fit2, trend =TRUE)
 
 DE_OCCC_vs_ccRCC_ratio <- topTable(fit2, coef="OCCC_vs_ccRCC_ratio", number=Inf, adjust.method="BH")
 DE_OCCC_vs_ccRCC_ratio <- cbind(Geneid = rownames(DE_OCCC_vs_ccRCC_ratio), DE_OCCC_vs_ccRCC_ratio)
 rownames(DE_OCCC_vs_ccRCC_ratio) <- NULL
 
 # save DE results
-#wb <- createWorkbook()
-#addWorksheet(wb, "OCCC_vs_ccRCC_ratio")
-#writeData(wb, "OCCC_vs_ccRCC_ratio", DE_OCCC_vs_ccRCC_ratio)
+wb <- createWorkbook()
+addWorksheet(wb, "OCCC_vs_ccRCC_ratio")
+writeData(wb, "OCCC_vs_ccRCC_ratio", DE_OCCC_vs_ccRCC_ratio)
 
-#write_csv(DE_OCCC_vs_ccRCC_ratio, file = "/Users/beyzaerkal/Desktop/occc_multi-omics/results/DE_results_OCCC_vs_ccRCC_ratio.csv")
+write_csv(DE_OCCC_vs_ccRCC_ratio, file = "/Users/beyzaerkal/Desktop/occc_multi-omics/results/DE_results_OCCC_vs_ccRCC_ratio.csv")
+
+
+
+
+####################################
+# Only OCCC and GTEx Ovary samples
 ####################################
 
-# Only OCCC and GTEx Ovary samples
 
-expr_mat_ovary <- cbind(occc_log, ovary_gtex_log)
-samples_ovary <- colnames(expr_mat_ovary)
+# using the already used expr_raw_oc_rc_filtered to subset only OCCC and GTEx Ovary samples for DE analysis -
+# to avoid reprocessing the raw data again
 
-study <- ifelse(samples_ovary %in% ea_samples, "Expression Atlas (2026)",
-                ifelse(samples_ovary %in% c("C1", "C2", "C3", "C4", "C5", "C6"), "Nagasawa et al. (2019)",
-                       ifelse(grepl("^CCC_", samples_ovary), "GSE189553",
-                              ifelse(grepl("^OVA[0-9]+", samples_ovary), "GSE160692",
-                                     ifelse(grepl("IGO_07456", samples_ovary), "Bolton et al. (2022)",
-                                                   ifelse(samples_ovary %in% ovary_samples, "GTEx_Ovary",NA))))))
+# matrix
+expr_mat_ovary <- expr_mat_oc_rc_filtered[, sample_info$Group %in% c("OCCC", "GTEx_Ovary")]
 
-
-
-group <- rep(NA, length(samples_ovary))
-names(group) <- samples_ovary
-group[samples_ovary %in% colnames(occc_log)] <- "OCCC"
-group[samples_ovary %in% colnames(ovary_gtex_log)] <- "GTEx_Ovary"
-
-
-source_type_ovary <- ifelse(samples_ovary %in% ea_samples,
-                            "Cell line",
-                            "Primary Tumour")
-
-# ovary and occc metadata only
-sample_info_ovary <- data.frame(
-  Sample = samples_ovary,
-  Study = factor(study),
-  Group = factor(group, levels = c("GTEx_Ovary", "OCCC")),
-  SourceType = factor(source_type_ovary,
-                      levels = c("Primary Tumour", "Cell line"))
-)
-rownames(sample_info_ovary) <- sample_info_ovary$Sample
-
+# metadata - and remove unused factors
+sample_info_ovary <- droplevels(sample_info[sample_info$Group %in% c("OCCC", "GTEx_Ovary"), ])
 all(colnames(expr_mat_ovary) == rownames(sample_info_ovary))
+
 # save
 #saveRDS(sample_info_ovary, file = "/Users/beyzaerkal/Desktop/occc_multi-omics/processed/sample_info_occc_ovary.rds")
 #saveRDS(expr_mat_ovary, file = "/Users/beyzaerkal/Desktop/occc_multi-omics/processed/expression_matrix_occc_ovary.rds")
@@ -402,7 +406,7 @@ contrast.matrix_ovary <- makeContrasts(OCCC_vs_GTEx_Ovary_ratio = OCCC - GTEx_Ov
 
 fit <- lmFit(expr_mat_ovary, design_ovary)
 fit2 <- contrasts.fit(fit, contrast.matrix_ovary)
-fit2 <- eBayes(fit2)
+fit2 <- eBayes(fit2, trend = TRUE)
 
 # add the geneid to columns
 DE_OCCC_vs_GTEx_Ovary_ratio <- topTable(fit2, coef="OCCC_vs_GTEx_Ovary_ratio", number=Inf, adjust.method="BH")
@@ -410,12 +414,57 @@ DE_OCCC_vs_GTEx_Ovary_ratio <- cbind(Geneid = rownames(DE_OCCC_vs_GTEx_Ovary_rat
 rownames(DE_OCCC_vs_GTEx_Ovary_ratio) <- NULL
 
 # save
-#write_csv(DE_OCCC_vs_GTEx_Ovary_ratio, file = "/Users/beyzaerkal/Desktop/occc_multi-omics/results/DE_results_OCCC_vs_GTEx_Ovary_ratio.csv")
+write_csv(DE_OCCC_vs_GTEx_Ovary_ratio, file = "/Users/beyzaerkal/Desktop/occc_multi-omics/results/DE_results_OCCC_vs_GTEx_Ovary_ratio.csv")
 
 
 # save them as excel sheet
-#addWorksheet(wb, "OCCC_vs_GTEx_Ovary_ratio")
-#writeData(wb, "OCCC_vs_GTEx_Ovary_ratio", DE_OCCC_vs_GTEx_Ovary_ratio)
+addWorksheet(wb, "OCCC_vs_GTEx_Ovary_ratio")
+writeData(wb, "OCCC_vs_GTEx_Ovary_ratio", DE_OCCC_vs_GTEx_Ovary_ratio)
 
-#saveWorkbook(wb, "/Users/beyzaerkal/Desktop/occc_multi-omics/supplementary/DE_results.xlsx", overwrite = TRUE)
+saveWorkbook(wb, "/Users/beyzaerkal/Desktop/occc_multi-omics/supplementary/DE_results.xlsx", overwrite = TRUE)
+
+
+
+
+##################
+# immune cell infiltration save the OCCC expr mat in tsv format (linear TPM - without log2tr)
+##################
+
+
+#CCOC_df_mRNA
+write_tsv(CCOC_df_mRNA, "/Users/beyzaerkal/Desktop/occc_multi-omics/inputs/CCOC_RNAseq_mRNA_TPM.tsv") # 200
+hist(as.numeric(as.matrix(CCOC_df_mRNA[,-1])))
+sum(duplicated(CCOC_df_mRNA$Geneid))
+
+# ea_tpm_mRNA
+hist(as.numeric(as.matrix(ea_tpm_mRNA[,-1])))
+sum(duplicated(ea_tpm_mRNA$Geneid))
+ea_tpm_mRNA <- ea_tpm_mRNA %>% group_by(Geneid) %>% summarise(across(where(is.numeric), mean, na.rm = TRUE), .groups = "drop")
+write_tsv(ea_tpm_mRNA, "/Users/beyzaerkal/Desktop/occc_multi-omics/inputs/E-MTAB_RNAseq_mRNA_TPM.tsv") # 7
+
+# tpm_GSE160692_mRNA
+write_tsv(tpm_GSE160692_mRNA, "/Users/beyzaerkal/Desktop/occc_multi-omics/inputs/GSE160692_RNA_seq_mRNA_TPM.tsv") # 11
+hist(as.numeric(as.matrix(tpm_GSE160692_mRNA[,-1])))
+sum(duplicated(tpm_GSE160692_mRNA$Geneid))
+
+# tpm_GSE189553_mRNA
+write_tsv(tpm_GSE189553_mRNA, "/Users/beyzaerkal/Desktop/occc_multi-omics/inputs/GSE189553_RNAseq_mRNA_TPM.tsv") # 11
+hist(as.numeric(as.matrix(tpm_GSE189553_mRNA[,-1])))
+sum(duplicated(tpm_GSE189553_mRNA$Geneid))
+
+# tpm_SD1_OC_mRNA
+write_tsv(tpm_SD1_OC_mRNA, "/Users/beyzaerkal/Desktop/occc_multi-omics/inputs/SD1_RNAseq_mRNA_TPM.tsv") # 6
+hist(as.numeric(as.matrix(tpm_SD1_OC_mRNA[,-1])))
+sum(duplicated(tpm_SD1_OC_mRNA$Geneid))
+
+
+
+
+
+
+
+
+
+
+
 
