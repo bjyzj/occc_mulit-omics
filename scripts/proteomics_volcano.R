5badee5f8c62ae14c4019100c6e00335768d2978
bjyzj <129678950+bjyzj@users.noreply.github.com>
2026-03-23 21:45:21 +0000

    change the threshold

--- scripts/proteomics_volcano.R
+++ scripts/proteomics_volcano.R
@@ -5,14 +5,19 @@ library(ggplot2)
 
 protDE <- read_csv("/Users/beyzaerkal/Desktop/occc_multi-omics/results/proteomics_results/DE_OCCC_vs_ccRCC_proteomics.csv")
 
+sum(protDE$adj.P.Val < 0.05, na.rm = TRUE)
+hist(protDE$logFC, breaks = 50)
+hist(protDE$adj.P.Val, breaks = 50)
 
-fc_thresh  <- 1 # 2-fold change threshold of +-1 in log2 scale
+summary(protDE$logFC)
+
+fc_thresh  <- 0.3 # 2-fold change threshold of +-1 in log2 scale
 fdr_thresh <- 0.05 # FDR cutoff
 
 volcano_df <- protDE %>% mutate(Significant = case_when(
   adj.P.Val < fdr_thresh & logFC > fc_thresh  ~ "Up",
   adj.P.Val < fdr_thresh & logFC < -fc_thresh ~ "Down", TRUE ~ "Not Significant"))
-
+table(volcano_df$Significant)
 
 ggplot(volcano_df, aes(x = logFC, y = -log10(adj.P.Val), color = Significant)) +
   geom_point(alpha = 0.6, size = 1.5) +
@@ -23,6 +28,7 @@ ggplot(volcano_df, aes(x = logFC, y = -log10(adj.P.Val), color = Significant)) +
   geom_hline(yintercept = -log10(fdr_thresh), linetype = "dashed") +
   labs(x = "log2 Fold Change", y = "-log10(FDR)") +
   theme_classic()
+
 
 
 
